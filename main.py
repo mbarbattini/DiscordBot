db31373fe8ca4b451204f6c4329ba061c16130d4
mbarbattini <mbarbattini@gmail.com>
2021-03-22 22:12:19 -0600

    encourage features

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import discord
 import os
 from dotenv import load_dotenv
+import requests
+import json
+import numbergame
+import random
 
 load_dotenv()
 
 client = discord.Client()
+
+sad_words = ['mean', 'depressed', 'unhappy', 'angry', 'miserable', 'depressing']
+
+starter_encouragements = [
+    'Cheer up!',
+    'Hang in there.',
+    'You are a great person!',
+    'You can do it!'
+]
+# inspirational quote from zenquotes.com
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+    return quote
 
 @client.event
 async def on_ready():
@@ -12,10 +31,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # do not reply to a bot
     if message.author == client.user:
         return
     
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    msg = message.content
 
+    if message.content.startswith('!inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    #if message.content.startswith('!hello'):
+    #    await message.channel.send('Hello!')
+
+    #if message.content.startswith('!numbergame'):
+    #    randomNumberGenerator(args)
+
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
+
+
+
+
+
+
+
+
+
+
+
+# login the bot
 client.run(os.getenv('TOKEN'))
